20fb40a8e6dc816b89a4cb270bf30eddbfbafb4d
Matthew Robison <matthew@thirdsundev.com>
2018-08-24 10:31:22 -0700

    fix: Adjust code to get service worker registered correctly

--- app/js/main.js
+++ app/js/main.js
@@ -15,12 +15,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 /**
 * Register Service Worker if browser supports it.
-*
- *registerServiceWorker = ()=> {
- *  if ('serviceWorker' in navigator) {
- *    navigator.serviceWorker.register('/sw.js', {scope: '/'});
- *  }
-}*/
+*/
+ registerServiceWorker = ()=> {
+   if ('serviceWorker' in navigator) {
+     navigator.serviceWorker.register('/sw.js');
+   }
+ }
 
 /**
  * Fetch all neighborhoods and set their HTML.
--- app/js/serviceWorker.js
+++
@@ -1,8 +0,0 @@
-/**
-* Register Service Worker if browser supports it.
-*/
-registerServiceWorker = ()=> {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js', {scope: '/'});
-  }
-}
